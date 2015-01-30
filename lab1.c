ec60c666f544b99c48f65fef1837204b01e44d5e
Dego <degozaru@live.com>
2015-01-30 13:44:01 -0500

    added bogo sort functionality

--- lab1.c
+++ lab1.c
@@ -12,6 +12,7 @@ int x[10];
 void initArray(int a[], int size, int min, int max, int seed);
 void printArray(int a[], int size, char name[]);
 void sortArray(int a[], int size);
+int sorted(int a[], int size);
 
 /***Main******************************/
 /**                                 **/
@@ -30,22 +31,40 @@ int main() {
 //This will print the array.
 //accepts an array, size, and a string to print before printing array.
 void printArray(int a[], int size, char name[]) {
+  int i;
   printf("%s:\n", name);
-  for(int i=0; i<size; i++)
+  for(i=0; i<size; i++)
     printf("%d\n", a[i]);
 } //End printArray()
 
 //This will initialize the array, populating it with random integers
 //user can specify size, minimum int, max int, and the seed to use.
 void initArray(int a[], int size, int min, int max, int seed) {
+  int i;
   srand(seed);
-  for(int i=0; i<size; i++) a[i] = (rand()%(max-min+1)) + min;
+  for(i=0; i<size; i++) a[i] = (rand()%(max-min+1)) + min;
 } //End initArray()
 
 //This will sort the given array.
 //Size and array must be specified.
-void sortArray(x, 10) {
-
+void sortArray(int a[], int size) {
+  int i, temp, r;
+  while(!sorted(a, size))
+    for(i=0; i<size; i++) {
+      r = rand()%size;
+      temp = a[i];
+      a[i] = a[r];
+      a[r] = temp;
+    }
 } //End sortArray()
+
+//This will check the array to see if it sorted.
+//returns 1 if sorted, 0 if not.
+int sorted(int a[], int size) {
+  int i;
+  for(i=0; i<size-1; i++)
+    if(a[i]>a[i+1]) return 0;
+  return 1;
+} //End sorted()
 /**                                 **/
 /*************************************/
