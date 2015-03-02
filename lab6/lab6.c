da0fd4c1735e8cde326a4381d57ecca0ccbb513a
Dego <degozaru@live.com>
2015-03-01 21:10:59 -0800

    lab6

--- lab6/lab6.c
+++ lab6/lab6.c
@@ -34,6 +34,9 @@ int main(void) {
 
 //This function will handle all initialization of the system.
 void Init() {
+  //Configure clock
+  RCC->AHBENR |= (1<<17);
+
   //Configure the interrupt to run every millisecond
   SysTick_Config(SystemCoreClock/1000);
 
