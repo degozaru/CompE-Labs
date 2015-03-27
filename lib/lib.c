e2a2bea45265e226ac8d5185f2be530b56a3fa15
Vincent Chan <chanv@rohan.sdsu.edu>
2015-03-26 22:23:51 -0700

    Update lib.c

--- lib/lib.c
+++ lib/lib.c
@@ -1,40 +1,142 @@
-/****************************
- * CompE375 Spring 2015
- * Library
- *
- * By Vincent Chan
- *
- * This is a library that will consolidate all config.
- * It will be added onto as time goes on.
- * ***************************/
-#ifndef LIBRARY_H
-#define LIBRARY_H
-#include <STM32F0xx.h>
+#include "lib.h"
 
-/*Pin mode definitions*/
-#define INPUT           0
-#define OUTPUT          1
-#define ALTMODE         2
+/**General Function***********************************************/
+/**                                                             **/
+/* This will enable the specified port. */
+void portEnable(char port) {
+  RCC->AHBENR |= (1<<(17+(port-'A')));
+} //End portEnable();
 
-/*Bit address for hardware*/
-#define HEXPAD          0
-#define SPEAKER         16
+/*This will set the pin to output 1.*/
+void pinHigh(char port, int pin) {
+  switch(port) {
+    case 'A':
+      GPIOA->BSRR |= (1<<pin);
+      break;
+    case 'B':
+      GPIOB->BSRR |= (1<<pin);
+      break;
+    case 'C':
+      GPIOC->BSRR |= (1<<pin);
+      break;
+    case 'D':
+      GPIOD->BSRR |= (1<<pin);
+      break;
+    case 'F':
+      GPIOF->BSRR |= (1<<pin);
+      break;
+  }
+} //End pinHigh()
 
-/*Basic port/GPIO functions*/
-void portEnable(char port);
-void pinHigh(char port, int pin);
-void pinLow(char port, int pin);
-void initPin(char port, int pin, int mode);
+/*This will reset the pin to output 0.*/
+void pinLow(char port, int pin) {
+  switch(port) {
+    case 'A':
+      GPIOA->BRR |= (1<<pin);
+      break;
+    case 'B':
+      GPIOB->BRR |= (1<<pin);
+      break;
+    case 'C':
+      GPIOC->BRR |= (1<<pin);
+      break;
+    case 'D':
+      GPIOD->BRR |= (1<<pin);
+      break;
+    case 'F':
+      GPIOF->BRR |= (1<<pin);
+      break;
+  }
+} //End pinLow()
 
-/*Hexpad*/
-void initHex();
-uint8_t butPress();
+/*This will initialize the pin*/
+void initPin(char port, int pin, int mode) {
+  switch(port) {
+    case 'A':
+      GPIOA->MODER |= (mode<<pin*2);
+      break;
+    case 'B':
+      GPIOB->MODER |= (mode<<pin*2);
+      break;
+    case 'C':
+      GPIOC->MODER |= (mode<<pin*2);
+      break;
+    case 'D':
+      GPIOD->MODER |= (mode<<pin*2);
+      break;
+    case 'F':
+      GPIOF->MODER |= (mode<<pin*2);
+      break;
+  }
+} //End initpin*2()
+/**                                                             **/
+/*****************************************************************/
 
-/*Speakers*/
-void initSpeaker();
-void speakerOn(uint16_t period, uint16_t compare1, uint16_t compare2);
-void speakerOff();
+/**HEXPAD FUNCTIONS***********************************************/
+/**                                                             **/
+/*This will initialize the hexpad for use.
+ * note that port C must be enabled before
+ * the hexpad will work.*/
+int row=0, col= 0;
+void initHex() {
+   /* Set fist 4 pins to output
+   * Set last 4 pins as pullup (input by default)*/
+  GPIOC->MODER |= (0x55<<HEXPAD); 
+  GPIOC->PUPDR |= (0x55<<(HEXPAD+4)*2);
+} //End initHex()
 
-#endif
+/*Returns the scancode if button is pressed
+ * Scancode: 4(row) + col + 1
+ * returns 0 if nothing is pressed*/
+uint8_t butPress() {
+	GPIOC->BSRR |= (0xf<<(HEXPAD));
+	for(row=0; row<4; row++) {
+    GPIOC->BRR |= (1<<(HEXPAD+row));
+    for(col=0; col<4; col++)
+      if(~GPIOC->IDR & (1<<((HEXPAD+4)+col))) {
+        GPIOC->BSRR |= (1<<(HEXPAD+row));
+        return (row*4) + col + 1;
+      }
+    GPIOC->BSRR |= (1<<(HEXPAD+row));
+  }
+  return 0;
+} //End butPress()
+/**                                                             **/
+/*****************************************************************/
 
+/**SPEAKER FUNCTIONS**********************************************/
+/**                                                             **/
+/*This enables the speaker for PWM output.
+ * Assuming a clock of 48 MHz:
+ * One tick is prescaled by 48, making it 1MHz,
+ * Port A's clock must be enabled.
+ * Pins PA8 and PA9 are used for PWM output */
+void initSpeaker() {
+	RCC->APB2ENR |= (1<<11);
+	TIM1->PSC = 48;
+	TIM1->CCER |= (0x55<<0);
+	TIM1->CCMR1 |= (0x60);
+	TIM1->CCMR1 |= (0x60<<8);
+	TIM1->BDTR |= (1<<15);
+  
+  GPIOA->MODER |= (0xA<<SPEAKER);
+  GPIOA->AFR[1] |= (0x22);
+} //End initSpeaker()
 
+/*Turns on the speaker with the specified period,
+ * compare1 goes to CCR1
+ * compare2 goes to CCR2 */
+void speakerOn(uint16_t period, uint16_t duty1, uint16_t duty2) {
+     period = ((SystemCoreClock/48) / (period)) - 1;
+     TIM1->ARR = period;
+     TIM1->CCR1 = (duty1 * period / 100);
+     TIM1->CCR2 = (duty2 * period / 100);
+     TIM1->CR1 |= (1<<0);
+} //End speakerOn()
+
+/*Turns off the speaker*/
+void speakerOff() {
+  TIM1->CR1 &= ~(1<<0);
+} //End speakerOff()
+/**                                                             **/
+/*****************************************************************/
